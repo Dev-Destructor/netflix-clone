b6743a9680435fa29828bedfdef6e6465a5a477c
Dev-Debobrata <dev.debobratapal@gmail.com>
2022-09-17 01:05:48 +0530

    added Rows component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,33 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Banner from '../components/Banner'
-import Header from '../components/Header'
+import Head from "next/head";
+import Banner from "../components/Banner";
+import Header from "../components/Header";
+import Rows from "../components/Rows";
+import { Movie } from "../typings";
+import requests from "../utils/requests";
 
-const Home: NextPage = () => {
+interface Props {
+  netflixOriginals: Movie[];
+  trendingNow: Movie[];
+  topRated: Movie[];
+  actionMovies: Movie[];
+  comedyMovies: Movie[];
+  horrorMovies: Movie[];
+  romanceMovies: Movie[];
+  documentaries: Movie[];
+  // products: Product[]
+}
+
+const Home = ({
+  netflixOriginals,
+  actionMovies,
+  comedyMovies,
+  documentaries,
+  horrorMovies,
+  romanceMovies,
+  topRated,
+  trendingNow,
+}: // products,
+Props) => {
   return (
     <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511]">
       <Head>
@@ -12,16 +36,68 @@ const Home: NextPage = () => {
         <link rel="icon" href="/netflix_icon.ico" />
       </Head>
 
-      <Header/>
-      <main>
-        <Banner />
+      <Header />
+      <main className="relative pl-4 pb-24 lg:space-y-24 lg:pl-16">
+        <Banner netflixOriginals={netflixOriginals} />
         <section>
-          {/* rows */}
+          <Rows title="Trending Now" movies={trendingNow} />
+          <Rows title="Top Rated" movies={topRated} />
+          <Rows title="Action Thrillers" movies={actionMovies} />
+          {/* My List */}
+          {/* {list.length > 0 && <Rows title="My List" movies={list} />} */}
+
+          <Rows title="Comedies" movies={comedyMovies} />
+          <Rows title="Scary Movies" movies={horrorMovies} />
+          <Rows title="Romance Movies" movies={romanceMovies} />
+          <Rows title="Documentaries" movies={documentaries} />
         </section>
       </main>
       {/* modal */}
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
+
+export const getServerSideProps = async () => {
+  // const products = await getProducts(payments, {
+  //   includePrices: true,
+  //   activeOnly: true,
+  // })
+  //   .then((res) => res)
+  //   .catch((error) => console.log(error.message));
+
+  const [
+    netflixOriginals,
+    trendingNow,
+    topRated,
+    actionMovies,
+    comedyMovies,
+    horrorMovies,
+    romanceMovies,
+    documentaries,
+  ] = await Promise.all([
+    fetch(requests.fetchNetflixOriginals).then((res) => res.json()),
+    fetch(requests.fetchTrending).then((res) => res.json()),
+    fetch(requests.fetchTopRated).then((res) => res.json()),
+    fetch(requests.fetchActionMovies).then((res) => res.json()),
+    fetch(requests.fetchComedyMovies).then((res) => res.json()),
+    fetch(requests.fetchHorrorMovies).then((res) => res.json()),
+    fetch(requests.fetchRomanceMovies).then((res) => res.json()),
+    fetch(requests.fetchDocumentaries).then((res) => res.json()),
+  ]);
+
+  return {
+    props: {
+      netflixOriginals: netflixOriginals.results,
+      trendingNow: trendingNow.results,
+      topRated: topRated.results,
+      actionMovies: actionMovies.results,
+      comedyMovies: comedyMovies.results,
+      horrorMovies: horrorMovies.results,
+      romanceMovies: romanceMovies.results,
+      documentaries: documentaries.results,
+      // products,
+    },
+  };
+};
