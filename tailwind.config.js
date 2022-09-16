a4329cf9b894ddc36b2c8192ba220335294cc8bf
Dev-Debobrata <dev.debobratapal@gmail.com>
2022-09-17 01:07:48 +0530

    added plugins

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,5 +7,5 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require('tailwindcss-textshadow'), require('tailwind-scrollbar-hide'), require('tailwind-scrollbar')],
 }
