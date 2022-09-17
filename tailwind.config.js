fd27665f5964709678ed574a8dd76d6be0e23f40
Dev-Debobrata <dev.debobratapal@gmail.com>
2022-09-17 23:50:10 +0530

    added custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,24 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      textShadow: {
+        md: "2px 2px 4px rgb(0 0 0 / 45%);",
+      },
+      backgroundImage: {
+        "gradient-to-b":
+          "linear-gradient(to bottom,rgba(20,20,20,0) 0,rgba(20,20,20,.15) 15%,rgba(20,20,20,.35) 29%,rgba(20,20,20,.58) 44%,#141414 88%,#141414 100%);",
+      },
+      colors: {
+        primary: "#141414",
+        secondary: "#e50914",
+        tertiary: "#e5e5e5",
+      },
+    },
   },
-  plugins: [require('tailwindcss-textshadow'), require('tailwind-scrollbar-hide'), require('tailwind-scrollbar')],
-}
+  plugins: [
+    require("tailwindcss-textshadow"),
+    require("tailwind-scrollbar-hide"),
+    require("tailwind-scrollbar"),
+  ],
+};
