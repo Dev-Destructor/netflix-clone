de78769f7def91ae87dd96906376fedfc6aa1724
Dev-Debobrata <dev.debobratapal@gmail.com>
2022-09-19 00:32:24 +0530

    handles myList

---
+++ hooks/useList.tsx
@@ -0,0 +1,29 @@
+import { collection, DocumentData, onSnapshot } from 'firebase/firestore'
+import { useEffect, useState } from 'react'
+import { db } from '../lib/firebase'
+import { Movie } from '../typings'
+
+function useList(uid: string | undefined) {
+  const [list, setList] = useState<DocumentData[] | Movie[]>([])
+
+  // connects the app to the firebase collection
+  useEffect(() => {
+    if (!uid) return
+
+    return onSnapshot(
+      collection(db, 'customers', uid, 'myList'),
+      (snapshot) => {
+        setList(
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            ...doc.data(),
+          }))
+        )
+      }
+    )
+  }, [db, uid])
+
+  return list
+}
+
+export default useList
