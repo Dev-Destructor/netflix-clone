3ee779630fe4e79a1210a3a8034ec6f1475abbb6
Dev-Debobrata <dev.debobratapal@gmail.com>
2022-09-18 23:50:46 +0530

    added next-transpile-modules to transpile

--- next.config.js
+++ next.config.js
@@ -1,11 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+// Using next-transpile-modules
+const withTM = require("next-transpile-modules")([
+  "@stripe/firestore-stripe-payments",
+]);
+
+module.exports = withTM({
   reactStrictMode: true,
   swcMinify: true,
   images: {
     domains: ["rb.gy", "image.tmdb.org"],
     dangerouslyAllowSVG: true,
   }
-}
-
-module.exports = nextConfig
+});
